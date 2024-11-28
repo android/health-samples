2abd87ad447cbd4c0ef824fd0b11c22a2f06262d
weichenc <weichenc@google.com>
2024-11-28 11:52:49 +0800

    add README

--- recording-api-on-mobile/RecordingApiOnMobileSample/app/src/main/java/com/example/recordingapionmobilesample/MainActivity.kt
+++ recording-api-on-mobile/RecordingApiOnMobileSample/app/src/main/java/com/example/recordingapionmobilesample/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.recordingapionmobilesample.helper.PermissionHelper
-import com.example.recordingapionmobilesample.ui.theme.RecordingAPIOnMobileSampleTheme
 import com.google.android.gms.fitness.FitnessLocal
 
 /**
