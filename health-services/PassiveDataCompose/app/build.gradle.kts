d91a190a5cd27f2cd8d62c964a4aa1bb4b36d6d6
Neelansh Sahai <neelanshsahai@google.com>
2025-05-27 16:42:35 +0530

    Migrate PassiveDataCompose to m3

---
+++ health-services/PassiveDataCompose/app/build.gradle.kts
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 35
+
+    namespace = "com.example.passivedatacompose"
+
+    defaultConfig {
+        applicationId = "com.example.passivedatacompose"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.majorVersion
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    // General compose dependencies
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.compose.ui.tooling)
+
+    implementation(libs.wear.compose.material3)
+    implementation(libs.wear.ui.tooling)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.compose.material.icons.extended)
+
+    // Health Services
+    implementation(libs.androidx.health.services)
+
+    // Used to bridge between Futures and coroutines
+    implementation(libs.guava)
+    implementation(libs.concurrent.futures)
+
+    // Used for WorkManager
+    implementation(libs.androidx.work)
+    implementation(libs.androidx.work.ktx)
+
+    // Used for Datastore
+    implementation(libs.androidx.datastore)
+
+    // Used for permissions
+    implementation(libs.accompanist.permissions)
+}
